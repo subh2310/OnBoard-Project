873d92006c9bf58d88041f8460acd1f1475d5607
Subhnesh Kumar <53490947+subh2310@users.noreply.github.com>
2022-11-09 10:41:04 +0530

    Updated urls.py file

--- foodhub/foodhub/urls.py
+++ foodhub/foodhub/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('accounts.urls')),
+    path('', include('accounts.urls')),
+    path("api-auth/", include("rest_framework.urls")),
+    path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('silk/', include('silk.urls', namespace='silk')),
 ]
-
 
 
 
