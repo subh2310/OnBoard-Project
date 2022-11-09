2a5e47f37c4ea809dbe77dde899f07bdc3c0ed0d
Subhnesh Kumar <53490947+subh2310@users.noreply.github.com>
2022-11-09 10:30:25 +0530

    Updated confest.py file
    Removed all code and added two fixture on it : merchant_data and consumer_data.

--- foodhub/tests/conftest.py
+++ foodhub/tests/conftest.py
@@ -1,61 +1,17 @@
 import pytest
-from rest_framework.test import APIClient
+from api.models import Profile
 from django.contrib.auth import get_user_model
-from accounts.models import Merchant, Store, Order, Item
-User=get_user_model()
 
-client = APIClient()
-@pytest.fixture
-def user():
-    payload= dict(
-        password1="subh@2310",
-        password2="subh@2310",
-        email="subhnesh0@gmail.com",
-        first_name="Subhnesh",
-        last_name="Kumar"
-    )
-    client.post('/account/register/', payload)
-
-
+User = get_user_model()
 
 @pytest.fixture
-def add_merchant():
-    merchant = Merchant.objects.create(
-        name='Kfc',
-        phone='9098786796',
-        email="kfc@gmail.com",
-    )
-    return merchant
+def merchant_data():
+    merchant = User.objects.create_user(username = "merchant", email = "merchant@gmail.com", password = "password@123")
+    profile = Profile.objects.create(user = merchant, name = "Merchant_User", role = 1)
+    return profile
 
 @pytest.fixture
-def add_item(add_merchant):
-    item = Item.objects.create(
-        name="Pizza",
-        cost=600,
-        merchant=add_merchant,
-        description="this is a Kfc shop",
-    )
-    return item
-@pytest.fixture
-def add_store(add_merchant, add_item):
-    store = Store.objects.create(
-        name='Bellandur kfc',
-        address='Bellandur green glen layout',
-        merchant=add_merchant,
-        items=add_item
-    )
-    return store
-
-
-@pytest.fixture
-def add_order(add_merchant, add_item, add_store):
-    order = Order.objects.create(
-        merchant=add_merchant,
-        store=add_store,
-        items=add_item,
-        total_cost=600  
-    )
-    return order
-
-
-
+def consumer_data():
+    consumer = User.objects.create_user(username = "consumer", email = "consumer@gmail.com", password = "password@123")
+    profile = Profile.objects.create(user = consumer, name = "consumer_User", role = 2)
+    return profile
