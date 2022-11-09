c09961e83a1995ef4b1f10a8b71dcf4ca5f14a3c
Subhnesh Kumar <53490947+subh2310@users.noreply.github.com>
2022-11-09 10:26:21 +0530

    Updated admin.py file.

--- foodhub/accounts/admin.py
+++ foodhub/accounts/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from accounts.models import Merchant, Item, Store, Order, User
-# Register your models here.
-
-models_iterable = [Merchant, Item, Store, Order, User]
-
-admin.site.register(models_iterable)
+from api.models import Items, Orders, Profile, Stores
+admin.site.register([Profile, Stores, Items, Orders])
