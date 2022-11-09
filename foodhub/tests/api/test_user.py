3b4d01e8b847dabb673153b09a1c173a50fd785e
Subhnesh Kumar <53490947+subh2310@users.noreply.github.com>
2022-11-09 10:33:22 +0530

    Updated tests.py file

--- foodhub/tests/api/test_user.py
+++ foodhub/tests/api/test_user.py
@@ -1,67 +1,65 @@
-from urllib import response
 import pytest
 from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
 
-from accounts.models import Merchant, Item, Store, Order
+from api.models import Items, Profile, Stores
 
-client=APIClient()
+client = APIClient()
+User = get_user_model()
 
+# Merchant Login & Registration Test Case
 @pytest.mark.django_db
-def test_register_user(client):
-    payload= dict(
-        password1="subh@2310",
-        password2="subh@2310",
-        email="subhnesh0@gmail.com",
-        first_name="Subhnesh",
-        last_name="Kumar"
-    )
-    response = client.post('/account/register/', payload)
-    data=response.data
-
-    assert data['first_name'] == payload['first_name']
+def test_merchant_registration():
+    payload = {"username" : "merchant", "email" : "merchant@gmail.com", "password" : "password@123", "profile.name" : "Merchant_User", "profile.role" : 1}
+    response = client.post("/register/", payload)
     assert response.status_code == 201
 
 @pytest.mark.django_db
-def test_login_user(user, client):
-    response = client.post('/account/login/', dict(email="subhnesh0@gmail.com", password="subh@2310"))
+def test_merchant_login(merchant_data):
+    response = client.post("/login/", dict(username = "merchant", password = "password@123"))
     assert response.status_code == 200
 
-
-
+# Stores Creation EndPoint Test Case
 @pytest.mark.django_db
-def test_merchant_creation_api_endpoint(user):
+def test_store_creation_api_endpoint(merchant_data):
     # import pdb; pdb.set_trace()
-    response = client.post("/account/login/", dict(email = "subhnesh0@gmail.com", password = "subh@2310"))
+    response = client.post("/login/", dict(username = "merchant", password = "password@123"))
     token = response.data['access']
-    payload= dict(
-        name = "Kfc", email ="kfc1@gmail.com", phone='789678987'
-    ) 
-    response=client.post("/account/merchants/", payload, **{'HTTP_AUTHORIZATION': f'Bearer {token}'})
+    req_send = client.post("/stores/", dict(name = "WhiteField Outlet", address = "WhiteField", lat = 13, lng = 13, merchant = merchant_data), **{'HTTP_AUTHORIZATION': f'Bearer {token}'})
+    assert req_send.status_code == 201
+
+# Items Creation Endpoint Test Case
+@pytest.mark.django_db
+def test_item_creation_api_endpoint(merchant_data):
+    # import pdb; pdb.set_trace()
+    response = client.post("/login/", dict(username = "merchant", password = "password@123"))
+    token = response.data['access']
+    store = Stores.objects.create(name = "WhiteField Outlet", address = "WhiteField", lat = 13, lng = 13, merchant = merchant_data)
+    req_send = client.post("/items/", dict(name = "Dosa", price = 120, description = "Sambhar Dosa", stores = store), **{'HTTP_AUTHORIZATION': f'Bearer {token}'})
+    assert req_send.status_code == 201
+
+
+# consumer Login & Registration Test Case
+@pytest.mark.django_db
+def test_consumer_registration():
+    payload = {"username" : "consumer", "email" : "consumer@gmail.com", "password" : "password@123", "profile.name" : "consumer_User", "profile.role" : 2}
+    response = client.post("/register/", payload)
     assert response.status_code == 201
 
 @pytest.mark.django_db
-def test_item_creation_api_endpoint(user,add_merchant):
-    # import pdb; pdb.set_trace()
-    response = client.post("/account/login/", dict(email = "subhnesh0@gmail.com", password = "subh@2310"))
-    token = response.data['access']
-    payload= dict(
-        name = "Pizza", merchant=add_merchant.id, cost=600
-    ) 
-    response=client.post("/account/items/", payload, **{'HTTP_AUTHORIZATION': f'Bearer {token}'})
-    assert response.status_code == 201
+def test_consumer_login(consumer_data):
+    response = client.post("/login/", dict(username = "consumer", password = "password@123"))
+    assert response.status_code == 200
 
+# Test Place Order Endpoints
 @pytest.mark.django_db
-def test_store_creation_api_endpoint(user, add_merchant, add_item):
+def test_place_order_api_endpoints(merchant_data, consumer_data):
     # import pdb; pdb.set_trace()
-    response = client.post("/account/login/", dict(email = "subhnesh0@gmail.com", password = "subh@2310"))
-    token = response.data['access']
-    req_send = client.post("/account/stores/", dict(name = "Bellandur Kfc Shop", address = "bellandur green glen layout", lat = 13, lng = 13, merchant = add_merchant.id, items=add_item.id), **{'HTTP_AUTHORIZATION': f'Bearer {token}'})
+    response = client.post("/login/", dict(username = "consumer", password = "password@123"))
+    token = response.data["access"]
+    store = Stores.objects.create(name = "WhiteField Outlet", address = "WhiteField", lat = 13, lng = 13, merchant = merchant_data)
+    item = Items.objects.create(name = "Dosa", price = 120, description = "Sambhar Dosa", stores = store)
+    req_send = client.post("/place_orders/", dict(user = consumer_data, merchant = merchant_data.pk, store = store.pk, items = item.pk), **{'HTTP_AUTHORIZATION': f'Bearer {token}'})
     assert req_send.status_code == 201
-
-@pytest.mark.django_db
-def test_order_creation_api_endpoint(user, add_merchant, add_store,  add_item,):
-    # import pdb; pdb.set_trace()
-    response = client.post("/account/login/", dict(email = "subhnesh0@gmail.com", password = "subh@2310"))
-    token = response.data['access']
-    req_send = client.post("/account/orders/", dict(merchant=add_merchant.id, items =add_item.id, store = add_store.id, total_cost=600), **{'HTTP_AUTHORIZATION': f'Bearer {token}'})
-    assert req_send.status_code == 201
+    items_present = set(Items.objects.all().values_list('name', flat=True))
+    assert "Dosa" in items_present
