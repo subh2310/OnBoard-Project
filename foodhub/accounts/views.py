d51fc4993e66707b9e45598b05bf245c9e42b3f8
Subhnesh Kumar <53490947+subh2310@users.noreply.github.com>
2022-11-09 10:23:54 +0530

    Updated views.py file
    Added more view on it like : SeeOrderView, ViewConsumerView, placeOrderView and UserChanged PasswordView and updated on items and Store view.

--- foodhub/accounts/views.py
+++ foodhub/accounts/views.py
@@ -1,239 +1,215 @@
+import structlog
+from .serializers import *
+from rest_framework import status
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
-from rest_framework import generics
-from accounts.models import (Merchant, Item, Store, Order, User)
-from accounts.serializers import (MerchantSerializer, ItemSerializer,
-                                  StoreSerializer, OrderSerializer, RegisterSerializer, LoginSerializer)
-from django.contrib.auth import authenticate
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
-from rest_framework import permissions
-from re import search
-from rest_framework import viewsets, filters, status
-import json
-import os
-from rest_framework.settings import api_settings
-from json.encoder import JSONEncoder
-from rest_framework.pagination import LimitOffsetPagination
-from django.http.response import JsonResponse
-from rest_framework.response import Response
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
-from accounts.tasks import create_order
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
-import structlog
-from rest_framework.utils.urls import remove_query_param, replace_query_param
-# Create your views here.
+from .tasks import create_store, create_order
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse_lazy
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from api.permissions import MerchantPermission, ConsumerPermission
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-log = structlog.get_logger()
+logger = structlog.get_logger(__name__)
+log = logger.new()
+logger_name = str(logger).upper()
 
-class RegisterView(APIView):
+
+def return_role(user):
+    p = Profile.objects.get(user=user)
+    if p.role == 1:
+        role = "Merchant"
+    elif p.role == 2:
+        role = "Customer"
+    return role
+
+
+@api_view(['GET'])
+def ApiHomepage(request, format=None):
+    log.info(event = "{} tries to access Home Page".format(request.user), user = request.user)
+    # log.info("Home Page", bar = "buzz")
+    return Response({
+        'Register': reverse_lazy('register', request=request, format=format),
+        'Login': reverse_lazy('login', request=request, format=format),
+        'Stores': reverse_lazy('stores', request=request, format=format),
+        'Items': reverse_lazy('items', request=request, format=format),
+        'View Consumers': reverse_lazy('view-consumer', request=request, format=format),
+        'View Orders': reverse_lazy('seeorders', request=request, format=format),
+        'Place Orders': reverse_lazy('placeorders', request=request, format=format),
+        'Change Password': reverse_lazy('change-password', request=request, format=format)
+    })
+
+
+class UserRegisterView(APIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = (AllowAny, )
+
     def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
+        serializer = self.serializer_class(data=request.data)
+        valid = serializer.is_valid(raise_exception=True)
+
+        if valid:
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            status_code = status.HTTP_201_CREATED
+            username = request.data['username']
+            password = request.data['password']
+            email = request.data['email']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            response = {
+                'success': True,
+                'statusCode': status_code,
+                'message': 'User successfully registered!',
+                'user': serializer.data
+            }
+            log.info("{} with {} email and {} role register.".format(username, email, return_role(user)), user = username)
+            return Response(response, status=status_code)
 
 
-class LoginView(APIView):
+class AuthUserLoginView(APIView):
+    serializer_class = UserLoginSerializer
+    permission_classes = (AllowAny, )
+
     def post(self, request):
-        serializer = LoginSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
+        valid = serializer.is_valid(raise_exception=True)
+
+        if valid:
+            status_code = status.HTTP_200_OK
+            username = request.data['username']
+            password = request.data['password']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            pk = User.objects.get(username=serializer.data['username']).pk
+            role = Profile.objects.get(user=pk).role
+            response = {
+                'success': True,
+                'statusCode': status_code,
+                'message': 'User logged in successfully',
+                'access': serializer.data['access'],
+                'refresh': serializer.data['refresh'],
+                'authenticatedUser': {
+                    'username': serializer.data['username'],
+                    'role': role
+                }
+            }
+            log.info("{} with {} role successfully login.".format(username, return_role(user)), user = request.user)
+            return Response(response, status=status_code)
+
+
+class UserChangePasswordView(generics.UpdateAPIView):
+    serializer_class = UserChangePasswordSerializer
+    model = User
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
         if serializer.is_valid():
-            user = authenticate(**serializer.validated_data)
-            if user:
-                if user.is_active:
-                    refresh = RefreshToken.for_user(user)
-                    access = AccessToken.for_user(user)
-                    return Response({
-                        'refresh': str(refresh),
-                        'access': str(access),
-                    })
-                return Response({'error': 'User is not active'}, status=status.HTTP_400_BAD_REQUEST)
-            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+            # Check old password
+            if not self.object.check_password(serializer.data.get("old_password")):
+                log.warning("{} with {} role not able to changed their password.".format(
+                self.request.user, return_role(self.request.user)), user = request.user)
+                return Response({"old_password": ["Wrong password."]}, status=status.HTTP_400_BAD_REQUEST)
+            # set_password also hashes the password that the user will get
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            response = {
+                'status': 'success',
+                'code': status.HTTP_200_OK,
+                'message': 'Password updated successfully'
+            }
+            log.info("{} with {} role successfully changed their password.".format(
+                self.request.user, return_role(self.request.user)), user = self.request.user.username)
+            return Response(response)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class MerchantViewSet(viewsets.ModelViewSet):
-    """
-    API view to perform CRUD operations on Merchant.
-    """
-    serializer_class = MerchantSerializer
-    queryset = Merchant.objects.all()
-    permission_classes = [IsAuthenticated]
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ['name']
+class StoresView(generics.ListCreateAPIView):
+    serializer_class = StoresSerializer
+    authentication_class = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated, MerchantPermission)
 
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        log.msg('Create Merchant Request', req=request.data)
-        return response
-    def update(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        log.msg('Update Merchant Request', req=request.data)
-        return response
+    def get_queryset(self):
+        profile = Profile.objects.get(user=self.request.user)
+        stores = Stores.objects.filter(merchant=profile)
+        if stores:
+            log.info("{} has accessed their stores list.".format(self.request.user.username), user = self.request.user.username)
+        else:
+            log.info("{} don't have any stores".format(self.request.user.username), user = self.request.user.username)
+        return stores
 
-    @action(detail=True)
-    def stores(self, request, pk=None):
-        merchant = Merchant.objects.filter(pk=pk).first()
-        stores = Store.objects.filter(merchant=merchant)
-        serializer = StoreSerializer(stores, many=True)
-        return Response(serializer.data)
-
-    @action(detail=True)
-    def items(self, request, pk=None):
-        merchant = Merchant.objects.filter(pk=pk).first()
-        items = Item.objects.filter(merchant=merchant)
-        serializer = ItemSerializer(items, many=True)
-        return Response(serializer.data)
-
-    @action(detail=True)
-    def orders(self, request, pk=None):
-        merchant = Merchant.objects.filter(pk=pk).first()
-        orders = Order.objects.filter(merchant=merchant)
-        serializer = OrderSerializer(orders, many=True)
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        pk = self.request.user.pk
+        create_store.delay(user=pk, data=serializer.data)
+        log.info("{} have added a new store.".format(self.request.user.username), user = self.request.user.username)
 
 
-class ItemViewSet(viewsets.ModelViewSet):
-    """
-    API view to perform CRUD operations on Item
-    """
-    serializer_class = ItemSerializer
-    queryset = Item.objects.select_related('merchant').all()
-    permission_classes = [IsAuthenticated]
+class ItemsView(generics.ListCreateAPIView):
+    serializer_class = ItemSerializers
+    authentication_class = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated, MerchantPermission)
 
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        log.msg('Create Item Request', req=request.data)
-        return response
-    def update(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        log.msg('Update Item Request', req=request.data)
-        return response
+    def get_queryset(self):
+        profile = Profile.objects.get(user=self.request.user)
+        items = Items.objects.filter(stores__merchant=profile)
+        if items:
+            log.info("{} has accessed their stores's item list.".format(self.request.user.username), user = self.request.user.username)
+        else:
+            log.info("{} don't have any items in their stores".format(self.request.user.username))
+        return items
 
+    def perform_create(self, serializer):
+        serializer.save()
+        log.info("{} merchant have added a new item in their store.".format(self.request.user.username), user = self.request.user.username)
 
-class StoreViewSet(viewsets.ModelViewSet):
-    """
-    API view to perform CRUD operations on Store
-    """
-    serializer_class = StoreSerializer
-    queryset = Store.objects.select_related(
-        'merchant').prefetch_related('items')
-    permission_classes = [IsAuthenticated]
-
-
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        log.msg('Create Store Request', req=request.data)
-        return response
-    def update(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        log.msg('Update Store Request', req=request.data)
-        return response
-
-    @action(detail=True)
-    def orders(self, request, pk=None):
-        store = Store.objects.filter(pk=pk).first()
-        orders = Order.objects.filter(store=store)
-        serializer = OrderSerializer(orders, many=True)
-        return Response(serializer.data)
-
-
-class OrderViewSet(viewsets.ModelViewSet):
-    """
-    API view to perform CRUD operations on Order
-    """
+class PlaceOrderView(generics.ListCreateAPIView):
     serializer_class = OrderSerializer
-    queryset = Order.objects.select_related(
-        'merchant', 'store').prefetch_related('items')
-    permission_classes = [IsAuthenticated]
-    pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        log.msg('Create Order Request', req=request.data)
-        return response
-    def update(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        log.msg('Update Order Request', req=request.data)
-        return response
+    authentication_class = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated, ConsumerPermission)
 
-    @property
-    def paginator(self):
-        """
-        The paginator instance associated with the view, or `None`.
-        """
-        user_agent = self.request.headers.get('User-Agent', None)
-        log.info('INSIDE paginator',user_agent = user_agent)
-        if not hasattr(self, '_paginator'):
-        
-            if self.pagination_class is None:
-                self._paginator = None
-            else:
-                if 'Postman' in user_agent:
-                    self._paginator = CustomPagination()
-                else:
-                    self._paginator = self.pagination_class()
-        return self._paginator
+    def get_queryset(self):
+        orders = Orders.objects.filter(user=self.request.user)
+        if orders:
+            log.info("{} have accessed their order's list.".format(self.request.user.username), user = self.request.user.username)
+        else:
+            log.info("{} have not placed any order yet.".format(self.request.user.username), user = self.request.user.username)
+        return orders
 
-    def create(self, request):
-        log.msg('Create Order Request', req=request.data)
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        create_order.apply_async(args=[serializer.data])
-        headers = self.get_success_headers(serializer.validated_data)
-        log.msg('Serializer Data', data=serializer.data,
-                validated_data=serializer.validated_data)
-        response = {"message": "Order is being placed"}
-        return Response(response, status=status.HTTP_201_CREATED,
-                        headers=headers)
+    def perform_create(self, serializer):
+        pk = self.request.user.pk
+        create_order.delay(user=pk, data=serializer.data)
+        log.info("{} have placed a new order.".format(self.request.user.username), user = self.request.user.username)
 
-class CustomPagination(LimitOffsetPagination):
-    default_limit = 10
-    limit_query_param = 'end'
-    offset_query_param = 'start'
 
-    def paginate_queryset(self, queryset, request, view=None):
-        self.count = self.get_count(queryset)
-        self.limit = self.get_limit(request)
-        if self.limit is None:
-            return None
+class SeeOrderView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+    authentication_class = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated, MerchantPermission)
 
-        self.offset = self.get_offset(request)
-        self.request = request
-        if self.count > self.limit and self.template is not None:
-            self.display_page_controls = True
+    def get_queryset(self):
+        user = User.objects.get(username=self.request.user)
+        profile = Profile.objects.get(user=user)
+        orders = Orders.objects.filter(merchant=profile)
+        if orders:
+            log.info("{} have accessed their order's list.".format(self.request.user.username), user = self.request.user.username)
+        else:
+            log.info("{} don't have any order placed yet.".format(self.request.user.username), user = self.request.user.username)
+        return orders
 
-        if self.count == 0 or self.offset > self.count:
-            return []
-        return list(queryset[self.offset : self.limit])
 
-    def get_next_link(self):
-        if self.limit >= self.count:
-            return None
+class ViewConsumerView(generics.ListAPIView):
+    serializer_class = ViewConsumerSerializer
+    authentication_class = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated, MerchantPermission)
 
-        url = self.request.build_absolute_uri()
-        url = replace_query_param(url, self.offset_query_param, self.limit)
-
-        offset = self.limit + self.default_limit
-        return replace_query_param(url, self.limit_query_param, offset)
-
-    def get_previous_link(self):
-        if self.offset <= 0:
-            return None
-
-        url = self.request.build_absolute_uri()
-        limit = self.offset
-        url = replace_query_param(url, self.limit_query_param, limit)
-
-        if self.offset - self.default_limit <= 0:
-            return remove_query_param(url, self.offset_query_param)
-
-        offset = self.offset - self.default_limit
-
-        return replace_query_param(url, self.offset_query_param, offset)
+    def get_queryset(self):
+        user = User.objects.filter(profile__role=2)
+        log.info("{} have accessed the registered consumer list".format(self.request.user.username), user = self.request.user.username)
+        return user
